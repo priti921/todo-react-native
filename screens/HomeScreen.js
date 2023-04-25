433b54217e35036b22f04654ca6ca8886a9647dc
Priti Roy <roypriti921@gmail.com>
2023-04-25 16:30:37 +0530

    added react navigation for navigation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const HomeScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>HELLO WORLD! IN MOBILE!</Text>      
+    </View>
+  );
+};
+
+export default HomeScreen;
