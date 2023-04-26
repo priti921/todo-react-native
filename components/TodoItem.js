b1ee18e4ff68960f61fff35cd515400b548d3dd5
Priti Roy <roypriti921@gmail.com>
2023-04-26 20:14:01 +0530

    added input secton and styles

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -11,7 +11,7 @@ const TodoItem = ({children, handleDelete}) => {
 
   return (
     <View className='bg-black p-4 rounded-md shadow-2xl my-2 flex-row align-center items-center'>
-      <Checkbox value={isChecked} onValueChange={() => setIsChecked(!isChecked)} className="p-3 mr-5" color={isChecked? 'black': 'white'}/>
+      <Checkbox value={isChecked} onValueChange={() => setIsChecked(!isChecked)}  className="p-2 mr-5" color={isChecked? 'black': 'white'}/>
       <Text className={`text-white text-xl tracking-widest ${isChecked? 'line-through italic': ''}`}>{children}</Text>
       {isChecked? (<Pressable className='ml-auto'>
         <MaterialIcons name="delete" size={24} color="white" onPress={handleDelete}/>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,43 @@
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, SafeAreaView, TextInput, Pressable } from 'react-native';
 import React,{useState} from 'react';
 import TodoItem from '../components/TodoItem';
-
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 const HomeScreen = () => {
   const [data, setData] = useState(['Eat','Sleep', 'Code']);
+  const [text, setText] = useState('');
 
   const handleDelete = (item)=>{
     const newData = data.filter((val) => val !== item);
     setData(newData);
     console.log(newData);
   }
+
+  const handleAdd = ()=>{
+    console.log(add)
+  }
   return (
-    <ScrollView className="px-5 py-16 bg-white">
+    <View className="bg-white h-full">
+    <View className="flex-row items-center justify-between mx-5 mt-5">
+    <TextInput
+        onChangeText={setText}
+        value={text}
+        placeholder="Add Todo"
+        keyboardType="default"
+        className="bg-gray-100 rounded-lg flex-1 mx-1 px-5 py-2"
+      />
+      <Pressable onPress={()=>handleAdd} className="bg-gray-100 rounded-lg px-2 py-2">
+        <MaterialIcons name="add" size={30} color="black" />
+      </Pressable>
+
+    </View>
+    <ScrollView className="px-5 py-8 bg-white">
       {
         data.map((item, index) => <TodoItem key={index} children={item} handleDelete={()=>handleDelete(item)}/>)
       }
       
     </ScrollView>
+      </View>
   );
 };
 
