330c6b6b9d0809d4e0da4c42a913e7c842d07ca3
Priti Roy <roypriti921@gmail.com>
2023-04-26 19:34:09 +0530

    added working delete button on checked

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,16 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import React, { useState } from 'react'
 import Checkbox from 'expo-checkbox';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
 //add checkbox [done]
 //add delete button
 
-const TodoItem = ({children}) => {
+const TodoItem = ({children, handleDelete}) => {
   const [ isChecked, setIsChecked ] = useState(false)
 
   return (
     <View className='bg-black p-4 rounded-md shadow-2xl my-2 flex-row align-center items-center'>
       <Checkbox value={isChecked} onValueChange={() => setIsChecked(!isChecked)} className="p-3 mr-5" color={isChecked? 'black': 'white'}/>
       <Text className={`text-white text-xl tracking-widest ${isChecked? 'line-through italic': ''}`}>{children}</Text>
+      {isChecked? (<Pressable className='ml-auto'>
+        <MaterialIcons name="delete" size={24} color="white" onPress={handleDelete}/>
+      </Pressable>) : null}
+      
     </View>
   )
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,20 @@
 import { View, Text, ScrollView } from 'react-native';
-import React from 'react';
+import React,{useState} from 'react';
 import TodoItem from '../components/TodoItem';
 
-let data = ['Eat','Sleep', 'Code']
 
 const HomeScreen = () => {
+  const [data, setData] = useState(['Eat','Sleep', 'Code']);
+
+  const handleDelete = (item)=>{
+    const newData = data.filter((val) => val !== item);
+    setData(newData);
+    console.log(newData);
+  }
   return (
     <ScrollView className="px-5 py-16 bg-white">
       {
-        data.map((item, index) => <TodoItem key={index} children={item} />)
+        data.map((item, index) => <TodoItem key={index} children={item} handleDelete={()=>handleDelete(item)}/>)
       }
       
     </ScrollView>
