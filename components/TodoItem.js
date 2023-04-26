97756616dfd32fbc07f8f556001c7c414ef5fee8
Priti Roy <roypriti921@gmail.com>
2023-04-26 20:27:18 +0530

    adding proper comments

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -7,12 +7,14 @@ import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 //add delete button
 
 const TodoItem = ({children, handleDelete}) => {
+  //sets state for checkbox
   const [ isChecked, setIsChecked ] = useState(false)
 
   return (
     <View className='bg-black p-4 rounded-md shadow-2xl my-2 flex-row align-center items-center'>
       <Checkbox value={isChecked} onValueChange={() => setIsChecked(!isChecked)}  className="p-2 mr-5" color={isChecked? 'black': 'white'}/>
       <Text className={`text-white text-xl tracking-widest ${isChecked? 'line-through italic': ''}`}>{children}</Text>
+      {/* show delete button if checkbox is checked */}
       {isChecked? (<Pressable className='ml-auto'>
         <MaterialIcons name="delete" size={24} color="white" onPress={handleDelete}/>
       </Pressable>) : null}
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,26 +4,29 @@ import TodoItem from '../components/TodoItem';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 const HomeScreen = () => {
-
+  //state for todo data and textInput value
   const [data, setData] = useState(['Eat','Sleep', 'Code']);
   const [text, setText] = useState('');
 
+
+  //deletes the item selected
   const handleDelete = (item)=>{
-    const newData = data.filter((val) => val !== item);
-    setData(newData);
+    const newData = data.filter((val) => val !== item);//filter out the item
+    setData(newData); //replacing with new data
     console.log(newData);
   }
 
+  //Adds new todo data
   const handleAdd = ()=>{
-    setData([...data, text]);
-    setText('');
+    setData([...data, text]);//adding new data
+    setText('');// setting textInput value to empty
   }
   return (
     <View className="bg-white h-full">
     <View className="flex-row items-center justify-between mx-5 mt-5">
     <TextInput
-        onChangeText={setText}
-        value={text}
+        onChangeText={setText} //set state on change
+        value={text} //get state value
         placeholder="Add Todo"
         keyboardType="default"
         className="bg-gray-100 rounded-lg flex-1 mx-1 px-5 py-2"
