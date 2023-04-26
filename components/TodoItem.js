2d4bd9c46e538816bf21fe1c5c88f11845ac0b2b
Priti Roy <roypriti921@gmail.com>
2023-04-26 18:54:26 +0530

    added expo supported checkbox

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,12 +1,16 @@
 import { View, Text } from 'react-native'
-import React from 'react'
-// add checkbox input
+import React, { useState } from 'react'
+import Checkbox from 'expo-checkbox';
+//add checkbox [done]
 //add delete button
 
 const TodoItem = ({children}) => {
+  const [ isChecked, setIsChecked ] = useState(false)
+
   return (
-    <View className='bg-slate-800 p-4 rounded-md shadow-2xl my-2'>
-      <Text className='text-white text-xl'>{children}</Text>
+    <View className='bg-black p-4 rounded-md shadow-2xl my-2 flex-row align-center items-center'>
+      <Checkbox value={isChecked} onValueChange={() => setIsChecked(!isChecked)} className="p-3 mr-5" color={isChecked? 'black': 'white'}/>
+      <Text className={`text-white text-xl ${isChecked? 'line-through italic': ''}`}>{children}</Text>
     </View>
   )
 }
