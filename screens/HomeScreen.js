4f22dbecb049fc3c08a5ab405d4802bb3e5eee2c
priti <roypriti921@gmail.com>
2023-04-29 18:26:10 +0530

    added icon, spalsh screen, and statusbar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,10 @@
 import { View, ScrollView } from "react-native";
 import React from "react";
 import { useAppContext } from "../data/context";
+import { StatusBar } from "expo-status-bar";
 import TodoItem from "../components/TodoItem";
 import InputTodo from "../components/InputTodo";
+
 
 /* 
 TODO: Add storage system
@@ -14,7 +16,7 @@ const HomeScreen = () => {
 
   return (
     <View className="bg-cyan-500 h-full">
-      
+     <StatusBar style="light"/> 
       {/* input todo */}
      <InputTodo/>
 
